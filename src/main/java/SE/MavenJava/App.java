88f692db030cca5f7dac7bbb844f25617db3cca4
Akhil <akhilakhil6107@gmail.com>
2025-12-03 08:56:56 +0530

    Add greeting message 'hai all!' to App.java

--- src/main/java/SE/MavenJava/App.java
+++ src/main/java/SE/MavenJava/App.java
@@ -6,5 +6,8 @@ package SE.MavenJava;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+          System.out.println("hai all!");
+        
+        
     }
 }
