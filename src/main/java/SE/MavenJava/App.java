0a1feac6985ca79cb221f5539e270f286bae3c28
Akhil <akhilakhil6107@gmail.com>
2025-12-03 09:26:23 +0530

    Remove duplicate 'hai all!' print statement

--- src/main/java/SE/MavenJava/App.java
+++ src/main/java/SE/MavenJava/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
           System.out.println("hai all!");
+         System.out.println("hai all!");
         
         
     }
